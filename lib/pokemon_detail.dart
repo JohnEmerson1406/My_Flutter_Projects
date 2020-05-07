3f06c321b70ba7c559c9ebc868b7c18703d617f1
john-emerson14 <john.carv.sousa@gmail.com>
2020-05-07 19:43:07 -0300

    feat: add image to detail page

--- lib/pokemon_detail.dart
+++ lib/pokemon_detail.dart
@@ -12,12 +12,24 @@ class PokeDetail extends StatelessWidget {
             height: MediaQuery.of(context).size.height / 1.5,
             width: MediaQuery.of(context).size.width - 20,
             left: 10,
-            top:  MediaQuery.of(context).size.height * 0.1,
+            top: MediaQuery.of(context).size.height * 0.1,
             child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  Text(pokemon.name),
+                  SizedBox(
+                    height: 70,
+                  ),
+                  Text(
+                    pokemon.name,
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                   Text("Height: ${pokemon.height}"),
                   Text("Weight: ${pokemon.weight}"),
                   Text("Types"),
@@ -60,6 +72,19 @@ class PokeDetail extends StatelessWidget {
                         .toList(),
                   ),
                 ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Hero(
+              tag: pokemon.img,
+              child: Container(
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.cover, image: NetworkImage(pokemon.img))),
               ),
             ),
           )
