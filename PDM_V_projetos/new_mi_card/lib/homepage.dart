0f607307bdfda18f23750f5462d95ee7bb2b1cf7
john-emerson14 <john.carv.sousa@gmail.com>
2020-05-22 16:04:47 -0300

    feat: add flip card

--- PDM_V_projetos/new_mi_card/lib/homepage.dart
+++ PDM_V_projetos/new_mi_card/lib/homepage.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flip_card/flip_card.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +22,112 @@ class HomePage extends StatelessWidget {
                 image: AssetImage("images/image.jpg"),
               ),
             ),
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 400,
+                    child: FlipCard(
+                      direction: FlipDirection.HORIZONTAL,
+                      front: Material(
+                        color: Colors.white.withOpacity(0.9),
+                        borderRadius: BorderRadius.circular(24),
+                        shadowColor: Colors.blue.withOpacity(0.2),
+                        child: _nameDetailsContainer(),
+                      ),
+                      back: Material(
+                        color: Colors.white.withOpacity(0.7),
+                        borderRadius: BorderRadius.circular(24),
+                        shadowColor: Colors.blue,
+                        child: _socialContainer(),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget _nameDetailsContainer() {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            height: 130,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: ListTile(
+                    leading: Container(
+                      width: 120,
+                      height: 120,
+                      decoration: new BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: new DecorationImage(
+                          fit: BoxFit.fill,
+                          image: new AssetImage('images/image2.jpg'),
+                        ),
+                      ),
+                    ),
+                    title: Padding(
+                      padding: const EdgeInsets.only(top: 32),
+                      child: Text(
+                        'John Emerson',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(top: 8),
+                      child: Text(
+                        'Flutter Developer',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            decoration: BoxDecoration(
+              color: Colors.orange.withOpacity(0.9),
+              borderRadius: BorderRadius.circular(24),
+            ),
+          ),
+          Container(),
+        ],
+      ),
+    );
+  }
+
+  Widget _socialContainer() {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            height: 130,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Colors.red.withOpacity(0.9),
+              borderRadius: BorderRadius.circular(24),
+            ),
+          ),
+          Container(),
         ],
       ),
     );
