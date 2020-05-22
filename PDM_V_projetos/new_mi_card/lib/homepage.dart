8585bddaa3782976928e5c1b8965692ee127985f
john-emerson14 <john.carv.sousa@gmail.com>
2020-05-22 15:23:43 -0300

    feat: add background image

--- PDM_V_projetos/new_mi_card/lib/homepage.dart
+++ PDM_V_projetos/new_mi_card/lib/homepage.dart
@@ -3,6 +3,22 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              image: new DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage("images/image.jpg"),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
