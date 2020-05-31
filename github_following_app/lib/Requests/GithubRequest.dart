f6ff8c8d281090e783bd656416400289418f1983
john-emerson14 <john.carv.sousa@gmail.com>
2020-05-31 16:42:36 -0300

    feat: add OAuth authentication

--- github_following_app/lib/Requests/GithubRequest.dart
+++ github_following_app/lib/Requests/GithubRequest.dart
@@ -3,8 +3,8 @@ import 'package:http/http.dart' as http;
 class Github {
   final String userName;
   final String url = 'https://api.github.com/';
-  static String client_id = '';
-  static String client_secret = '';
+  static String client_id = '2a68e031a6fbf2876bd3';
+  static String client_secret = 'e54d9c7848b12999ad20ad2f9e4a5a0076d8542d';
 
   final String query = '?client_id=${client_id}&client_secret=${client_secret}';
 
