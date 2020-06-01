ed580ad39bcc36e13e7c43a1690b5ccc26f8903d
john-emerson14 <john.carv.sousa@gmail.com>
2020-06-01 19:19:57 -0300

    fix: listen false in provider

--- github_following_app/lib/main.dart
+++ github_following_app/lib/main.dart
@@ -13,16 +13,16 @@ void main() => runApp(ChangeNotifierProvider<UserProvider>(
 
 class HomePage extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _StateHomePage createState() => _StateHomePage();
 }
 
-class _HomePageState extends State<HomePage> {
+class _StateHomePage extends State<HomePage> {
   TextEditingController _controller = TextEditingController();
 
   void _getUser() {
     if (_controller.text == '') {
-      Provider.of<UserProvider>(context)
-          .setMessage('Please inter your username');
+      Provider.of<UserProvider>(context, listen: false)
+          .setMessage('Please Enter your username');
     } else {
       Provider.of<UserProvider>(context)
           .fetchUser(_controller.text)
@@ -38,72 +38,84 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.black,
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 100,
-              ),
-              Container(
-                width: 80,
-                height: 80,
-                child: CircleAvatar(
-                  backgroundColor: Colors.transparent,
-                  backgroundImage: AssetImage('images/github-icon-png-29.jpg'),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          color: Colors.black,
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 100,
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Text(
-                'Github',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 150,
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.white.withOpacity(.1),
-                ),
-                child: TextField(
-                  controller: _controller,
-                  style: TextStyle(color: Colors.white),
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Github username',
-                    hintStyle: TextStyle(color: Colors.grey),
+                Container(
+                  width: 80,
+                  height: 80,
+                  child: CircleAvatar(
+                    backgroundColor: Colors.transparent,
+                    backgroundImage:
+                        AssetImage('images/github-icon-png-29.jpg'),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              MaterialButton(
-                padding: EdgeInsets.all(20),
-                color: Colors.blue,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
+                SizedBox(
+                  height: 30,
                 ),
-                child: Align(
-                  child: Text(
-                    'Get Your Following Now',
+                Text(
+                  "Github",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 150,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.white.withOpacity(.1)),
+                  child: TextField(
+                    onChanged: (value) {
+                      Provider.of<UserProvider>(context).setMessage(null);
+                    },
+                    controller: _controller,
+                    enabled: !Provider.of<UserProvider>(context).isLoading(),
                     style: TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                        errorText:
+                            Provider.of<UserProvider>(context).getMessage(),
+                        border: InputBorder.none,
+                        hintText: "Github username",
+                        hintStyle: TextStyle(color: Colors.grey)),
                   ),
                 ),
-                onPressed: () {
-                  _getUser();
-                },
-              ),
-            ],
+                SizedBox(
+                  height: 20,
+                ),
+                MaterialButton(
+                  padding: EdgeInsets.all(20),
+                  color: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Align(
+                    child: Provider.of<UserProvider>(context).isLoading()
+                        ? CircularProgressIndicator(
+                            backgroundColor: Colors.white,
+                            strokeWidth: 2,
+                          )
+                        : Text(
+                            'Get Your Following Now',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                  ),
+                  onPressed: () {
+                    _getUser();
+                  },
+                )
+              ],
+            ),
           ),
         ),
       ),
