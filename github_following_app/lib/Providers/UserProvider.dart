6f4ccdc458447b45bb949dabb78b93cd6a1f92e1
john-emerson14 <john.carv.sousa@gmail.com>
2020-06-01 18:13:23 -0300

    feat: add methods to UserProvider

--- github_following_app/lib/Providers/UserProvider.dart
+++ github_following_app/lib/Providers/UserProvider.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:github_following_app/Models/User.dart';
+import 'package:github_following_app/Requests/GithubRequest.dart';
 
 class UserProvider with ChangeNotifier {
   User user;
@@ -8,10 +11,47 @@ class UserProvider with ChangeNotifier {
 
   Future<bool> fetchUser(username) async {
     setLoading(true);
+
+    await Github(username).fetchUser().then((data) {
+      if (data.statusCode == 200) {
+        setUser(User.fromJson(json.decode(data.body)));
+      } else {
+        Map<String, dynamic> result = json.decode(data.body);
+        setMessage(result['message']);
+      }
+    });
+
+    return isUser();
   }
 
   void setLoading(value) {
     loading = value;
     notifyListeners();
+  }
+
+  bool isLoading() {
+    return loading;
+  }
+
+  void setUser(value) {
+    user = value;
+    notifyListeners();
+  }
+
+  User getUser() {
+    return user;
+  }
+
+  void setMessage(value) {
+    errorMessage = value;
+    notifyListeners();
+  }
+
+  String getMessage() {
+    return errorMessage;
+  }
+
+  bool isUser() {
+    return user != null ? true : false;
   }
 }
