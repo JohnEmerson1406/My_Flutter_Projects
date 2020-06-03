6879253929afb642a2ddf23319510e5da2739af6
john-emerson14 <john.carv.sousa@gmail.com>
2020-06-02 21:31:00 -0300

    feat: add users avatars

--- github_following_app/lib/Pages/Following.dart
+++ github_following_app/lib/Pages/Following.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:github_following_app/Models/User.dart';
 import 'package:github_following_app/Providers/UserProvider.dart';
+import 'package:github_following_app/Requests/GithubRequest.dart';
 import 'package:provider/provider.dart';
 
 class FollowingPage extends StatefulWidget {
@@ -16,6 +19,13 @@ class _FollowingPageState extends State<FollowingPage> {
   Widget build(BuildContext context) {
     setState(() {
       user = Provider.of<UserProvider>(context).getUser();
+
+      Github(user.login).fetchFollowing().then((following) {
+        Iterable list = json.decode(following.body);
+        setState(() {
+          users = list.map((model) => User.fromJson(model)).toList();
+        });
+      });
     });
     return Scaffold(
       body: Container(
@@ -56,6 +66,53 @@ class _FollowingPageState extends State<FollowingPage> {
                   ),
                 ),
               ),
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate([
+                Container(
+                  height: 470,
+                  child: users != null
+                      ? ListView.builder(
+                          scrollDirection: Axis.vertical,
+                          itemCount: users.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              padding: EdgeInsets.all(15),
+                              decoration: BoxDecoration(
+                                  border: Border(
+                                      bottom:
+                                          BorderSide(color: Colors.grey[200]))),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: <Widget>[
+                                      Container(
+                                        width: 60,
+                                        height: 60,
+                                        child: CircleAvatar(
+                                          backgroundImage: NetworkImage(
+                                              users[index].avatar_url),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        )
+                      : Container(
+                          child: Align(
+                            child: Text('Data is loading...'),
+                          ),
+                        ),
+                ),
+              ]),
             ),
           ],
         ),
