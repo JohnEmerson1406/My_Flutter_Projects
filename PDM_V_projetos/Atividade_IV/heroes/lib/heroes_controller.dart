800d00a52635f37978944c6d5ae475e57a0935dd
JohnEmerson1406 <john.carv.sousa@gmail.com>
2020-08-31 19:42:30 -0300

    feat: add list of heroes

---
+++ PDM_V_projetos/Atividade_IV/heroes/lib/heroes_controller.dart
@@ -0,0 +1,11 @@
+import 'package:heroes/hero_model.dart';
+
+class HeroesController {
+  List<HeroModel> heroes = [
+    HeroModel(name: "Thor"),
+    HeroModel(name: "Iron man"),
+    HeroModel(name: "Batman"),
+    HeroModel(name: "Captain america"),
+    HeroModel(name: "Super man"),
+  ];
+}
