44de0547112f46e2dd94daf48a7c31d5efe3010c
JohnEmerson1406 <john.carv.sousa@gmail.com>
2020-10-09 17:08:27 -0300

    feat: add detail screen

--- PDM_V_projetos/cookbook/lib/main.dart
+++ PDM_V_projetos/cookbook/lib/main.dart
@@ -59,9 +59,39 @@ class _MyHomePageState extends State<MyHomePage> {
         itemBuilder: (context, index) {
           return ListTile(
             title: Text(todos[index].title),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailScreen(todo: todos[index]),
+                ),
+              );
+            },
           );
         },
       )
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  // Declare a field that holds the Todo.
+  final Todo todo;
+
+  // In the constructor, require a Todo.
+  DetailScreen({Key key, @required this.todo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Use the Todo to create the UI.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todo.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text(todo.description),
+      ),
     );
   }
 }
