65a0578edc7107eadf7cb6a9a7edc4c46c38de1e
JohnEmerson1406 <john.carv.sousa@gmail.com>
2020-10-18 11:31:01 -0300

    feat: add click to Users

--- PDM_V_projetos/usersapi/lib/main.dart
+++ PDM_V_projetos/usersapi/lib/main.dart
@@ -54,8 +54,45 @@ class _MyListScreenState extends State {
         body: ListView.builder(
           itemCount: users.length,
           itemBuilder: (context, index) {
-            return ListTile(title: Text(users[index].name));
+            return ListTile(
+              title: Text(users[index].name),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailScreen(user: users[index]),
+                  ),
+                );
+              },
+            );
           },
         ));
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  final User user;
+
+  DetailScreen({Key key, @required this.user}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(user.name),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Name: ' + user.name),
+            Text('Username: ' + user.username),
+            Text('Email: ' + user.email)
+          ],
+        ),
+      ),
+    );
   }
 }
