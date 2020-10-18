5cef61d31a7240fefceeefd225d7aee6f1b2f430
JohnEmerson1406 <john.carv.sousa@gmail.com>
2020-10-18 11:37:13 -0300

    refactor: add detail.dart

--- PDM_V_projetos/usersapi/lib/main.dart
+++ PDM_V_projetos/usersapi/lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:usersapi/API.dart';
+import 'package:usersapi/detail.dart';
 import 'package:usersapi/models/User.dart';
 import 'package:flutter/material.dart';
 
@@ -48,50 +49,24 @@ class _MyListScreenState extends State {
   @override
   build(context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("User List"),
-        ),
-        body: ListView.builder(
-          itemCount: users.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(users[index].name),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DetailScreen(user: users[index]),
-                  ),
-                );
-              },
-            );
-          },
-        ));
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  final User user;
-
-  DetailScreen({Key key, @required this.user}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
       appBar: AppBar(
-        title: Text(user.name),
+        title: Text("User List"),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text('Name: ' + user.name),
-            Text('Username: ' + user.username),
-            Text('Email: ' + user.email)
-          ],
-        ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(users[index].name),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailScreen(user: users[index]),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
