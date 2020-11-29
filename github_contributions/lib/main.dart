48b6926227669c422658408e6cd6dd581c6a6b59
JohnEmerson1406 <john.carv.sousa@gmail.com>
2020-11-28 21:53:20 -0300

    feat: add main code

--- github_contributions/lib/main.dart
+++ github_contributions/lib/main.dart
@@ -1,4 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' show parse;
+
+class Contribution {
+  int count;
+  String color;
+  String date;
+
+  Contribution({this.count, this.color, this.date});
+
+  Contribution.fromJson(Map<String, dynamic> json) {
+    count = json['count'];
+    color = json['color'];
+    date = json['date'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['count'] = this.count;
+    data['color'] = this.color;
+    data['date'] = this.date;
+    return data;
+  }
+}
+
+Future<List<Contribution>> getContributions(
+  String login, {
+  String from,
+  String to,
+}) async {
+  var url = 'https://github.com/$login';
+  if (from != null && to != null) {
+    url += '?from=$from&to=$to';
+  }
+  var res = await http.get(url);
+  var document = parse(res.body);
+  var rectNodes =
+      document.querySelector('.js-calendar-graph-svg').querySelectorAll('rect');
+  return rectNodes.map((rectNode) {
+    return Contribution(
+      color: rectNode.attributes['fill'],
+      count: int.tryParse(rectNode.attributes['data-count']),
+      date: rectNode.attributes['data-date'],
+    );
+  }).toList();
+}
+
+/// Get user contributions data as svg string
+Future<String> getContributionsSvg(
+  String login, {
+  bool keepDateText = false,
+  String from,
+  String to,
+}) async {
+  var url = 'https://github.com/$login';
+  if (from != null && to != null) {
+    url += '?from=$from&to=$to';
+  }
+  var res = await http.get(url);
+  var document = parse(res.body);
+  var svgNode = document.querySelector('.js-calendar-graph-svg');
+
+  if (!keepDateText) {
+    // remove text tags
+    svgNode.children[0].children.forEach((child) {
+      if (child.localName == 'text') {
+        child.remove();
+      }
+    });
+
+    // resize
+    // the size depend on if use check the 'Activity overview' option
+    if (svgNode.attributes['width'] == '563') {
+      svgNode.attributes['width'] = '528';
+      svgNode.attributes['height'] = '68';
+      svgNode.children[0].attributes['transform'] = 'translate(-11, 0)';
+    } else {
+      svgNode.attributes['width'] = '637';
+      svgNode.attributes['height'] = '84';
+      svgNode.children[0].attributes['transform'] = 'translate(-13, 0)';
+    }
+  }
+
+  return svgNode?.outerHtml;
+}
 
 void main() {
   runApp(MyApp());
@@ -52,16 +137,34 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _incrementCounter() async {
+    var login = 'JohnEmerson1406'; // replace this with GitHub account you want
+
+    // Get the contribution of a certain year
+    // If it is the past year, from: yyyy-12-01, to: yyyy-12-31
+    // if not, from: The first day of the current month, to: today
+    // var contributions = await getContributions(login, from: '2020-08-01', to: '2020-08-04');
+    // print(contributions[0].color);
+
+    // get svg string
+    // String svg = await getContributionsSvg(login);
+    // print(svg);
+
+    // get color and count of this year's contribution
+    // var contributions1 = await getContributions(login);
+    // print(contributions1[0].color);
   }
+
+  // void _incrementCounter() {
+  //   setState(() {
+  //     // This call to setState tells the Flutter framework that something has
+  //     // changed in this State, which causes it to rerun the build method below
+  //     // so that the display can reflect the updated values. If we changed
+  //     // _counter without calling setState(), then the build method would not be
+  //     // called again, and so nothing would appear to happen.
+  //     _counter++;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
