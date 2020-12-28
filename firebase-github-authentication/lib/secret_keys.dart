c2eb740b758fc28487489935376283ea01866066
John Emerson <john.carv.sousa@gmail.com>
2020-12-28 20:10:47 -0300

    feat: add client secret and delete old projects

---
+++ firebase-github-authentication/lib/secret_keys.dart
@@ -0,0 +1,6 @@
+library secretkeys;
+
+const GITHUB_CLIENT_ID = "f2296d289c93912418e1";
+const GITHUB_CLIENT_SECRET = "0f72fd5ec50fd11e991bfcd9e4cfeea6ade4a069";
+
+// https://github-login-teste.firebaseapp.com/__/auth/handler
